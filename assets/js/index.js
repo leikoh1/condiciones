8d1d5cb890920a214311db48f22c498ea96eff40
Leonardo Sanchez <leonardo.sei2005@gmail.com>
2024-06-08 18:12:38 -0400

    se agrega el proyecto

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+let img = document.querySelector("img")
+let borde = false;
+
+img.addEventListener('click' , ()=>{
+    if (borde == false){
+    document.querySelector("img").style.border = "solid 2px green"
+    borde = true
+    }else{
+        document.querySelector("img").style.border =""
+        borde = false
+        }
+})
